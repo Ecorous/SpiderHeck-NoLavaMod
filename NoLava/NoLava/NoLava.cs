67cb6b1d1f653466d6d03d2345f8d31d6f370b96
niiK0 <37949374+niiK0@users.noreply.github.com>
2022-06-05 10:35:37 +0100

    Added source files

---
+++ NoLava/NoLava/NoLava.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using BepInEx;
+using HarmonyLib;
+
+namespace NoLava
+{
+    [BepInPlugin(ModName, ModGUID, ModVersion)]
+    public class NoLava : BaseUnityPlugin
+    {
+        public const string ModName = "NoLava";
+        public const string ModAuthor = "niiK0";
+        public const string ModGUID = "com.niiko.nolava";
+        public const string ModVersion = "1.0.1";
+        private readonly Harmony harmony = new Harmony(ModGUID);
+
+        void Awake()
+        {
+            // Applying patches
+            harmony.PatchAll();
+            Logger.LogInfo($"{ModName} successfully loaded! Made by {ModAuthor}");
+        }
+
+        [HarmonyPatch(typeof(DeathZone), "Start")]
+        public class DeathZone_Remove
+        {
+
+            [HarmonyPrefix]
+            private static bool DisableLava()
+            {
+                FindObjectOfType<DeathZone>().gameObject.SetActive(false);
+                return false;
+            }
+        }
+    }
+}
